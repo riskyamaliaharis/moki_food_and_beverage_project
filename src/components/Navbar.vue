c8e7fc8db27c152c2a604bfa95961c15948b0b97
Risky Amalia Haris <amaliaharisr@gmail.com>
2020-12-18 14:59:11 +0700

    add Navbar Category

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm navbar-expand-xs navbar-light "
+  >
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav ">
+        <li class="nav-item">
+          <a class="nav-link on" href="#">Favorite Product</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Coffee</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Non-Coffee</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Foods</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Add-on</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style>
+.navbar-nav a.on {
+  font-weight: 700;
+  border-bottom-style: ridge;
+  border-bottom-color: #895537;
+}
+</style>
