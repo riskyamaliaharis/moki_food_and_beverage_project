a4d04f07e6ba54946d73159828a06d7b50ec0e57
Risky Amalia Haris <amaliaharisr@gmail.com>
2021-02-14 16:08:00 +0700

    UPDATE get promo

--- src/components/Coupon.vue
+++ src/components/Coupon.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="left-side">
     <b-button v-b-toggle.sidebar-1 class="btn-coupon">Get Promo</b-button>
-    <b-sidebar id="sidebar-1" title="Promo for you" shadow>
+    <b-sidebar width="400px" id="sidebar-1" title="Promo for you" shadow>
       <div class="px-3 py-2 show-card-promo">
         <div
           :class="i % 2 === 0 ? 'card brown' : 'card orange'"
           v-for="(el, i) in cards"
           :key="i"
         >
-          <b-row align-v="baseline">
-            <b-col cols="5">
+          <i
+            class="fa fa-close 3x"
+            style="position: absolute;  margin-left: 320px; cursor: pointer"
+          ></i>
+          <b-row align-v="center">
+            <b-col cols="4">
               <img
                 v-if="el.image_src"
                 :src="'http://localhost:3000/' + el.image_src"
@@ -23,7 +27,16 @@
                 class="image"
               />
             </b-col>
-            <b-col cols="7">{{ el }}</b-col>
+            <b-col cols="7" class="coupon-info">
+              <p class="name">{{ el.product_name.toUpperCase() }}</p>
+              <h5>Discount {{ el.coupon_discount * 100 }}%</h5>
+              <p>
+                FROM {{ formatTime1(el.start_coupon) }} UNTIL
+                {{ formatTime1(el.end_coupon) }}
+              </p>
+              <p>Expired {{ formatTime(el.end_coupon) }}</p>
+              <p>{{ el.promo_description }}</p>
+            </b-col>
           </b-row>
         </div>
       </div>
@@ -33,7 +46,7 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-
+import moment from 'moment'
 export default {
   data() {
     return {}
@@ -45,11 +58,30 @@ export default {
     ...mapGetters({ cards: 'getAllCoupons' })
   },
   methods: {
-    ...mapActions(['getPromosVuex'])
+    ...mapActions(['getPromosVuex']),
+    formatTime(value) {
+      moment.locale('en')
+      return moment(String(value))
+        .endOf('day')
+        .fromNow()
+    },
+    formatTime1(value) {
+      moment.locale('en')
+      return moment(String(value)).format('MMM Do YY')
+    }
   }
 }
 </script>
 <style scoped>
+.coupon-info p {
+  font-size: 13px;
+  margin-bottom: 10px;
+}
+.coupon-info p.name {
+  letter-spacing: 5px;
+  font-weight: 800;
+  font-size: 20px;
+}
 div.left-side .btn-coupon {
   background-color: white;
   width: 106px;
@@ -67,9 +99,9 @@ div.show-card-promo {
 }
 
 .card {
-  height: 125px;
+  /* height: 125px; */
   margin-bottom: 20px;
-  padding: 7px;
+  padding: 10px 15px;
   border-radius: 10px;
 }
 .brown {
@@ -81,8 +113,8 @@ div.show-card-promo {
   color: black;
 }
 .image {
-  width: 107px;
-  height: 107px;
+  width: 90px;
+  height: 90px;
   object-fit: cover;
   border-radius: 30px;
 }
